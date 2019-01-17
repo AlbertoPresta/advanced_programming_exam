f25075aaea7adfb4d4fe77e293316f9290282a10
apresta <presta.alberto@libero.it>
2019-01-17 10:47:42 +0100

    iteratori

--- c++/Tree.h
+++ c++/Tree.h
@@ -54,9 +54,95 @@ public:
     struct Constiterator;
     iterator find(const T& t);
     bool trova(const T& t);
-    
+    iterator First(){return iterator{first};}
+    iterator Last(){return iterator{last};}
+    iterator Root(){return iterator{root};}
+    iterator End(){return iterator{nullptr};}
   
     
 };
 
+//definisco la classe iteratore fuori della classe Tree
+template<typename T ,typename W>
+struct Tree<T,W>::iterator{
+    using Node = Tree<T,W>::Node;
+    Node* current;
+    iterator(Node* p):
+    current{p} {}
+    
+    
+    T parent_key(){
+        if(current->parent==nullptr) {
+            cout<<"no parents,we are in the root!"<<endl;
+            return current->key;
+        }
+        return current->parent->key;
+    }
+    iterator& operator++(){
+        if(current->right!=nullptr){ //cerca se può scendere a destra
+            current=current->right;
+            if(current->left!=nullptr){
+                while(current->left!=nullptr){  //da qui scende a sinistra fin tanto possibile
+                    current=current->left;
+                }
+            }
+            return *this;
+        }
+        else {
+            if(current->key  <  current->parent->key) { //se può risale a destra
+                current=current->parent;
+                return *this;
+            }
+            else {
+                while(current->key  >  current->parent->key) { //in alternativa risale a sinistra
+                    current=current->parent;
+                };
+                current=current->parent;  //infine sale a destra
+                return *this;
+            };
+            
+        };
+        return *this;
+    }
+    iterator& operator--(){
+        if(current->left!=nullptr){ //cerca se può scendere a destra
+            current=current->left;
+            if(current->right!=nullptr){
+                while(current->right!=nullptr){  //da qui scende a sinistra fin tanto possibile
+                    current=current->right;
+                }
+            }
+            return *this;
+        }
+        else {
+            if(current->key  >  current->parent->key) { //se può risale a destra
+                current=current->parent;
+                return *this;
+            }
+            else {
+                while(current->key  <  current->parent->key) { //in alternativa risale a sinistra
+                    current=current->parent;
+                };
+                current=current->parent;  //infine sale a destra
+                return *this;
+            };
+            
+        };
+        return *this;
         
+    }
+    T& operator*() {
+        return current->key;
+    }
+    T& operator!(){
+        return current->value;
+    }
+    
+    
+    
+    bool operator==(const iterator& b){return current==b.current;}
+    bool operator!=(const iterator& b){return current!=b.current;}
+    
+   
+};
+
