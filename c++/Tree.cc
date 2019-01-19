9c00ea3bcbfb0c692046e848bae7fcca890bbd4d
apresta <presta.alberto@libero.it>
2019-01-19 15:54:09 +0100

    move

--- c++/Tree.cc
+++ c++/Tree.cc
@@ -1,7 +1,6 @@
-//7
 
 #include<iostream>
-#include<memory>
+#include<utility>
 
 using std::string;
 using std::cout;
@@ -11,11 +10,11 @@ using std::endl;
 
 template<typename T,typename W>
 class Tree{
-
+    
     struct Node{
         T key;
         W value;
-        Node* left; 
+        Node* left;
         Node* right;
         Node* parent;
         Node (T& k, W& v, Node* l=nullptr, Node* r=nullptr,Node* p=nullptr):
@@ -23,12 +22,12 @@ class Tree{
         ~Node() {left=nullptr;right=nullptr;parent=nullptr;}
         
         /*
-        //copy constructor per il nodo
-        Node(const Tree<T, W>::Node& n):
-            key{n.key}, value{n.value}, left{nullptr}, right{nullptr}, lparent{nullptr},rparent{nullptr} {}*/
-       
+         //copy constructor per il nodo
+         Node(const Tree<T, W>::Node& n):
+         key{n.key}, value{n.value}, left{nullptr}, right{nullptr}, lparent{nullptr},rparent{nullptr} {}*/
+        
     };
-
+    
     Node* first;
     Node* root;
     Node* last;
@@ -39,7 +38,7 @@ class Tree{
         if(n->left==nullptr and n->right==nullptr) return;
         if(n->left!=nullptr) ctr_insert(t,n->left);
         if(n->right!=nullptr)ctr_insert(t,n->right);}
-
+    
     
 public:
     
@@ -52,9 +51,7 @@ public:
     template<typename OT,typename OW>
     friend std::ostream& operator<<(std::ostream&, const Tree<OT,OW>&);
     Tree(): //costruttore dell'albero
-        first{nullptr},root{nullptr},last{nullptr},size_tree{0} {}
-        
-        
+    first{nullptr},root{nullptr},last{nullptr},size_tree{0} {}
     
     
     
@@ -66,19 +63,19 @@ public:
         
         
         bool FIRST{true}; //booleano che serve a identificare l'ultimo nodo a sinistra, first
-        bool LAST{true};  
+        bool LAST{true};
         
         while(!(p->left ==nullptr && p->right==nullptr) && !(p->left==nullptr && k<p->key) && !(p->right==nullptr && k>p->key)) {
             if(k<p->key) {p=p->left;LAST=false;} //scende a sinistra. Se lo fa una volta sicuramente non è l'ultimo elemento
             else {p=p->right;FIRST=false;};      //speculare, se scende a destra una volta certamente non è il first
-            };
+        };
         //per la condizione del while, p è l'ultimo nodo preesistente
-         
-        Node* elem=new Node{k,v,nullptr,nullptr,p}; //inserisce il nuovo nodo con p parente 
+        
+        Node* elem=new Node{k,v,nullptr,nullptr,p}; //inserisce il nuovo nodo con p parente
         
         if(k<p->key) {p->left=elem; if (FIRST==true) first=elem;} //aggiorna il ramo sinistro di p
         else {p->right=elem; if (LAST==true) last=elem; };          //aggiorna il ramo destro di p
-
+        
         size_tree++; //aggiornamento grandezza albero
     }
     
@@ -88,16 +85,16 @@ public: //forse va cancellato
     struct iterator{
         Node* current;
         iterator(Node* p):
-            current{p} {}
+        current{p} {}
         
         //metodi di iterator!
         //metodo begin--> ritorn l'iteratore first,cioè quello con la chiave più piccola
         iterator begin(){
             return iterator{first};
-            }
+        }
         //metodo end-->ritorna l'iteratore dopo last--> NON SICURO
         iterator end(){
-           return iterator{nullptr};
+            return iterator{nullptr};
         }
         
         // ritorna l'iteratore con chiave più grande
@@ -109,61 +106,61 @@ public: //forse va cancellato
             return iterator{root};
         }
         iterator& operator++(){
-                if(current->right!=nullptr){ //cerca se può scendere a destra
-                    current=current->right;
-                    if(current->left!=nullptr){
-                        while(current->left!=nullptr){  //da qui scende a sinistra fin tanto possibile
-                            current=current->left;
-                            }
-                        }
-                    return *this;
+            if(current->right!=nullptr){ //cerca se può scendere a destra
+                current=current->right;
+                if(current->left!=nullptr){
+                    while(current->left!=nullptr){  //da qui scende a sinistra fin tanto possibile
+                        current=current->left;
                     }
-                else {
-                    if(current->key  <  current->parent->key) { //se può risale a destra
-                        current=current->parent;
-                        return *this;
-                     }
-                     else {
-                        while(current->key  >  current->parent->key) { //in alternativa risale a sinistra
-                            current=current->parent;
-                            };
-                            current=current->parent;  //infine sale a destra
-		                       return *this;
-		                   };
-            
-		               };
-		               return *this;    
-		               }
-        iterator& operator--(){  
-         if(current->left!=nullptr){ //cerca se può scendere a destra
-                    current=current->left;
-                    if(current->right!=nullptr){
-                        while(current->right!=nullptr){  //da qui scende a sinistra fin tanto possibile
-                            current=current->right;
-                            }
-                        }
+                }
+                return *this;
+            }
+            else {
+                if(current->key  <  current->parent->key) { //se può risale a destra
+                    current=current->parent;
                     return *this;
-                    }
+                }
                 else {
-                    if(current->key  >  current->parent->key) { //se può risale a destra
+                    while(current->key  >  current->parent->key) { //in alternativa risale a sinistra
                         current=current->parent;
-                        return *this;
-                     }
-                     else {
-                        while(current->key  <  current->parent->key) { //in alternativa risale a sinistra
-                            current=current->parent;
-                            };
-                            current=current->parent;  //infine sale a destra
-		                       return *this;
-		                   };
+                    };
+                    current=current->parent;  //infine sale a destra
+                    return *this;
+                };
+                
+            };
+            return *this;
+        }
+        iterator& operator--(){
+            if(current->left!=nullptr){ //cerca se può scendere a destra
+                current=current->left;
+                if(current->right!=nullptr){
+                    while(current->right!=nullptr){  //da qui scende a sinistra fin tanto possibile
+                        current=current->right;
+                    }
+                }
+                return *this;
+            }
+            else {
+                if(current->key  >  current->parent->key) { //se può risale a destra
+                    current=current->parent;
+                    return *this;
+                }
+                else {
+                    while(current->key  <  current->parent->key) { //in alternativa risale a sinistra
+                        current=current->parent;
+                    };
+                    current=current->parent;  //infine sale a destra
+                    return *this;
+                };
+                
+            };
+            return *this;
             
-		               };
-		               return *this;
-        
         }
         T operator*() {
             return current->key;
-            }
+        }
         T operator!(){
             if(current->parent==nullptr) return 10000;
             return current->parent->key;
@@ -182,10 +179,10 @@ public: //forse va cancellato
     //CONST ITERATOR
     
     struct Constiterator{
-         const Node* current;
+        const Node* current;
         Constiterator(const Node* p): current{p} {}
-    
-    
+        
+        
         Constiterator cbegin() const {
             return Constiterator{first};
         }
@@ -201,20 +198,20 @@ public: //forse va cancellato
             return Constiterator{last};
         }
         T operator*() {
-          return current->key;
-            }
+            return current->key;
+        }
         T operator!(){
             if(current->parent==nullptr) return 10000;
-          return current->parent->key;
-            }
+            return current->parent->key;
+        }
         
         bool operator==(const Constiterator& b){
             return current==b.current;
-            }
+        }
         bool operator!=(const Constiterator& b){
             return current!=b.current;
-            }
-            
+        }
+        
         Constiterator& operator++(){
             {
                 if(current->right!=nullptr){ //cerca se può scendere a destra
@@ -222,101 +219,120 @@ public: //forse va cancellato
                     if(current->left!=nullptr){
                         while(current->left!=nullptr){  //da qui scende a sinistra fin tanto possibile
                             current=current->left;
-                            }
                         }
-                    return *this;
                     }
+                    return *this;
+                }
                 else {
                     if(current->key  <  current->parent->key) { //se può risale a destra
                         current=current->parent;
                         return *this;
-                     }
-                     else {
+                    }
+                    else {
                         while(current->key  >  current->parent->key) { //in alternativa risale a sinistra
                             current=current->parent;
-                            };
-                            current=current->parent;  //infine sale a destra
-		                       return *this;
-		                   };
-            
-		               };
-		               return *this;    
-		               }
-		               };
-       Constiterator& operator--(){  
-         if(current->left!=nullptr){ //cerca se può scendere a destra
-                    current=current->left;
-                    if(current->right!=nullptr){
-                        while(current->right!=nullptr){  //da qui scende a sinistra fin tanto possibile
-                            current=current->right;
-                            }
-                        }
-                    return *this;
-                    }
-                else {
-                    if(current->key  >  current->parent->key) { //se può risale a destra
-                        current=current->parent;
+                        };
+                        current=current->parent;  //infine sale a destra
                         return *this;
-                     }
-                     else {
-                        while(current->key  <  current->parent->key) { //in alternativa risale a sinistra
-                            current=current->parent;
-                            };
-                            current=current->parent;  //infine sale a destra
-		                       return *this;
-		                   };
+                    };
+                    
+                };
+                return *this;
+            }
+        };
+        Constiterator& operator--(){
+            if(current->left!=nullptr){ //cerca se può scendere a destra
+                current=current->left;
+                if(current->right!=nullptr){
+                    while(current->right!=nullptr){  //da qui scende a sinistra fin tanto possibile
+                        current=current->right;
+                    }
+                }
+                return *this;
+            }
+            else {
+                if(current->key  >  current->parent->key) { //se può risale a destra
+                    current=current->parent;
+                    return *this;
+                }
+                else {
+                    while(current->key  <  current->parent->key) { //in alternativa risale a sinistra
+                        current=current->parent;
+                    };
+                    current=current->parent;  //infine sale a destra
+                    return *this;
+                };
+                
+            };
+            return *this;
             
-		               };
-		               return *this;
+        };
         
-        };
-            
         ~Constiterator()= default;
-        };
+    };
     
     
     
     //FINE CONST ITERATOR
     //METODO FIND
-   iterator find(const T t){
-       Node* j{root};
-       while(j!=nullptr){
-         
-           if(j->key == t){
-               cout<<"key found!"<<endl;
-               return j;
-           }
-           else{
-               if(j->key > t){
-                 
-                   j=j->left;
-                  
-               }
-               
-               else{
-                 
-                    j=j->right;
-                  
-               }
-           }
-       }
-       cout<< "key not found!"<<endl;
-       return j;
-   }
-   
-
-   //copy semantics
-    Tree (const Tree<T,W>& t):   //è da implementare la costruzione mediante il metodo insert
-        first{nullptr},
-        root {nullptr},
-        last{nullptr},
-        size_tree{0}  {
-            ctr_insert(*this,t.root);
+    iterator find(const T t){
+        Node* j{root};
+        while(j!=nullptr){
             
+            if(j->key == t){
+                cout<<"key found!"<<endl;
+                return j;
             }
-   
-
-
+            else{
+                if(j->key > t){
+                    
+                    j=j->left;
+                    
+                }
+                
+                else{
+                    
+                    j=j->right;
+                    
+                }
+            }
+        }
+        cout<< "key not found!"<<endl;
+        return j;
+    }
+    
+    
+    //copy semantics
+    Tree (const Tree<T,W>& t):   //è da implementare la costruzione mediante il metodo insert
+    first{nullptr},
+    root {nullptr},
+    last{nullptr},
+    size_tree{0}  {
+        ctr_insert(*this,t.root);
+    }
+    
+    Tree& operator=(const Tree<T,W>& t) {
+        Clear();
+        ctr_insert(*this,t.root);
+        cout<<"chiamato assignment"<<endl;
+        return *this;
+    }
+    
+    Tree& operator=(Tree<T,W>&& t) {
+        Clear();
+        root=t.root;
+        first=t.first;
+        last=t.last;
+        size_tree=t.size_tree;
+        t.root=nullptr;
+        t.first=nullptr;
+        t.last=nullptr;
+        t.size_tree=0;
+        cout<<"chiamato move"<<endl;
+        return *this;
+    }
+    
+    
     void recursive_clear(Node* n){
         Node* l{n->left};
         Node* r{n->right};
@@ -325,7 +341,7 @@ public: //forse va cancellato
         if(l==nullptr and r==nullptr) return;
         if(l!=nullptr) recursive_clear(l);
         if(r!=nullptr) recursive_clear(r);
-        }
+    }
     void Clear() {
         if(size_tree==0) return;
         Node* n{root};
@@ -334,59 +350,48 @@ public: //forse va cancellato
         last=nullptr;
         recursive_clear(n);
         size_tree=0;
-        }
-
-
-     
-//infine metto il distruttore di default
+    }
+    
+    
+    
+    //infine metto il distruttore di default
     ~Tree() {Clear();}
     
     
-
-void rec_balance(int s, Tree<T,W>& b, Tree<T,W>::iterator m, int lun) {
     
-    b.Insert(*m,9999);
-    Node* p{b.Root()};
-    /*if (b.Size()>9){
-    Constiterator iter{b.First()};
-    cout<<"Albero provvisorio"<<endl;
-    for(int k{1};k<10;k++) {cout<<*iter<<" "<<" parent"<<!iter<<endl; ++iter;}
-    cout<<endl;};*/
-    if(b.Size()==s) return;
-    
-    int lun_l{lun/2};
-    int lun_r{lun-1-lun_l};
-    iterator i{m};
-    iterator j{m};
-    
-    if(lun_l>0) {
-        for(int ii{1};ii<=lun_l/2+lun_l%2;ii++) --i;
-        cout<<endl;
-        rec_balance(s,b,i,lun_l);
+    void rec_balance(int s, Tree<T,W>& b, Tree<T,W>::iterator m, int lun) {
+        
+        b.Insert(*m,9999);
+        Node* p{b.Root()};
+        if(b.Size()==s) return;
+        
+        int lun_l{lun/2};
+        int lun_r{lun-1-lun_l};
+        iterator i{m};
+        iterator j{m};
+        
+        if(lun_l>0) {
+            for(int ii{1};ii<=lun_l/2+lun_l%2;ii++) --i;
+            cout<<endl;
+            rec_balance(s,b,i,lun_l);
         };
-    if(lun_r>0) {
-        for(int jj{0};jj<=lun_r/2;jj++) ++j;
-        rec_balance(s,b,j,lun_r);
+        if(lun_r>0) {
+            for(int jj{0};jj<=lun_r/2;jj++) ++j;
+            rec_balance(s,b,j,lun_r);
         };
-    return;  
-}
-
-
-void balance() {
-    cout<<root->key<<endl;
-    Tree<T,W> b;
-    iterator i{first};
-    for(int ii{1};ii<size_tree/2+1;ii++) ++i;
-    rec_balance(size_tree,b,i,size_tree);
+        return;
+    }
     
-    root=b.Root();
-    first=b.First();
-    last=b.Last();
-    cout<<root->key<<endl;
-    cout<<first->key<<endl;
-    cout<<last->key<<endl;
     
-}
+    void balance() {
+        cout<<root->key<<endl;
+        Tree<T,W> b;
+        iterator i{first};
+        for(int ii{1};ii<size_tree/2+1;ii++) ++i;
+        rec_balance(size_tree,b,i,size_tree);
+        *this=std::move(b);
+        cout<<"stampa "<<*this<<endl;
+    }
     
     
     
@@ -396,45 +401,50 @@ template<typename T,typename W>
 std::ostream& operator<<(std::ostream& os, const Tree<T,W>& l) {
     if (l.Size()==0) {cout<<"empty tree"<<endl; return os;}
     else {
-    typename Tree<T,W>::Constiterator j = l.first;
-    for(; j!=l.last;++j){
-        os <<"key: "<<*j<<"   parent: " <<!j<<endl;
+        typename Tree<T,W>::Constiterator j = l.first;
+        for(; j!=l.last;++j){
+            os <<"key: "<<*j<<"   parent: " <<!j<<endl;
         }
-    os <<"key: "<<*j<<"   parent: " <<!j<<endl;
-
-    return os ;}
+        os <<"key: "<<*j<<"   parent: " <<!j<<endl;
+        
+        return os ;}
 }
 
 
 
 
 int main() {
-
-
-
-
-
-Tree<int, long long int> A;
-A.Insert(1,1);
-A.Insert(2,2);
-A.Insert(3,3);
-A.Insert(4,5);
-A.Insert(5,0);
-A.Insert(6,0);
-A.Insert(7,0);
-A.Insert(8,0);
-A.Insert(9,0);
-A.Insert(10,0);
-A.Insert(11,0);
-A.Insert(12,0);
-A.Insert(15,0);
-A.Insert(14,0);
-A.Insert(13,0);
-//cout<<A<<endl;
-A.print();
-A.balance();
-A.print();
-cout<<endl<<A<<endl;
-
-
+    
+    
+    
+    
+    
+    Tree<int, long long int> A;
+    A.Insert(1,1);
+    A.Insert(2,2);
+    A.Insert(3,3);
+    A.Insert(4,5);
+    A.Insert(5,0);
+    A.Insert(6,0);
+    A.Insert(7,0);
+    A.Insert(8,0);
+    A.Insert(9,0);
+    A.Insert(10,0);
+    A.Insert(11,0);
+    A.Insert(12,0);
+    A.Insert(15,0);
+    A.Insert(14,0);
+    A.Insert(13,0);
+    //cout<<A<<endl;
+    A.print();
+    A.balance();
+    A.print();
+    cout<<endl<<A<<endl;
+    
+    Tree<int, long long int> B{A};
+    B.Insert(16,2);
+    cout<<B<<endl;
+    A=B;
+    cout<<A<<endl;
+    
 }
