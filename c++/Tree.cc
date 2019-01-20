502579117e389d79b7d4a527f34a3877ce3b0769
Andrea Gasparin <andrea.gasparin.87@gmail.com>
2019-01-20 18:15:21 +0100

    sistemazione balance_fast

--- c++/Tree.cc
+++ c++/Tree.cc
@@ -1,11 +1,27 @@
-//7
-
+//12
 #include<iostream>
 #include<memory>
-
+#include<utility>
+#include<cmath>
+#include<vector>
 using std::string;
 using std::cout;
 using std::endl;
+
+
+/*   mancancti!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-const e no except
+-ced e annesso operatore ++
+-test
+-test con oggetti
+-ultimo punto
+-error handling
+    -caso chiavi uguali
+    -vedere se metto oggetti, cioè se non è def l'overloading dell'operatore <
+    
+-aiuto devo pushare gli eserizi, e a sto punto i rivediamo
+
+*/
 
 //IN QUESTO  CODICE SI IMPLEMENTA I CONSTITERATOR INDIPENDENTEMENTE DAGLI ITERATOR--> MOLTO SCOMODO VISTO CHE IL CODICE DIVENTA INCREDIBILMENTE LUNGO: ITERATOR E CONST ITERATOR SONO PRATICAMENTE UGUALI! PROVO A FARE CLASSE EREDITATA!
 
@@ -33,13 +49,7 @@ class Tree{
     Node* root;
     Node* last;
     unsigned int size_tree; //grandezza dell'albero
-    
-    void ctr_insert(Tree& t, Node* n) {
-        t.Insert(n->key, n->value);
-        if(n->left==nullptr and n->right==nullptr) return;
-        if(n->left!=nullptr) ctr_insert(t,n->left);
-        if(n->right!=nullptr)ctr_insert(t,n->right);}
-
+        
     
 public:
     
@@ -54,8 +64,6 @@ public:
     Tree(): //costruttore dell'albero
         first{nullptr},root{nullptr},last{nullptr},size_tree{0} {}
         
-        
-    
     
     
     
@@ -164,14 +172,13 @@ public: //forse va cancellato
         T operator*() {
             return current->key;
             }
-        T operator!(){
-            if(current->parent==nullptr) return 10000;
-            return current->parent->key;
+        W operator!(){
+            return current->value;
         }
         bool operator==(const iterator& b){return current==b.current;}
         bool operator!=(const iterator& b){return current!=b.current;}
         
-        Node* node() const{return current;}
+        Node* node() {return current;}
     };
     
     
@@ -182,8 +189,8 @@ public: //forse va cancellato
     //CONST ITERATOR
     
     struct Constiterator{
-         const Node* current;
-        Constiterator(const Node* p): current{p} {}
+          Node* current;
+        Constiterator( Node* p): current{p} {}
     
     
         Constiterator cbegin() const {
@@ -203,9 +210,8 @@ public: //forse va cancellato
         T operator*() {
           return current->key;
             }
-        T operator!(){
-            if(current->parent==nullptr) return 10000;
-          return current->parent->key;
+        W operator!(){
+          return current->value;
             }
         
         bool operator==(const Constiterator& b){
@@ -214,6 +220,11 @@ public: //forse va cancellato
         bool operator!=(const Constiterator& b){
             return current!=b.current;
             }
+        
+        T parent_key()  {
+            if(current->parent==nullptr) {cout<<"we are in the root"<<endl; return current->key;};
+            return current->parent->key;
+        }
             
         Constiterator& operator++(){
             {
@@ -270,6 +281,7 @@ public: //forse va cancellato
 		               return *this;
         
         };
+        Node* node() {return current;}
             
         ~Constiterator()= default;
         };
@@ -278,32 +290,28 @@ public: //forse va cancellato
     
     //FINE CONST ITERATOR
     //METODO FIND
-   iterator find(const T t){
-       Node* j{root};
-       while(j!=nullptr){
-         
-           if(j->key == t){
+    iterator find(const T t){
+        Node* j{root};
+        while(j!=nullptr){
+            if(j->key == t){
                cout<<"key found!"<<endl;
-               return j;
-           }
-           else{
-               if(j->key > t){
-                 
-                   j=j->left;
-                  
-               }
-               
-               else{
-                 
-                    j=j->right;
-                  
-               }
-           }
-       }
-       cout<< "key not found!"<<endl;
-       return j;
-   }
-   
+               return j;}
+            else{
+               if(j->key > t){j=j->left;}
+               else{j=j->right;}}
+            }
+        cout<< "key not found!"<<endl;
+        return j;
+        }
+private:
+    void ctr_insert(Tree& t, Node* n) {
+        t.Insert(n->key, n->value);
+        if(n->left==nullptr and n->right==nullptr) return;
+        if(n->left!=nullptr) ctr_insert(t,n->left);
+        if(n->right!=nullptr)ctr_insert(t,n->right);}
+
+
+public: 
 
    //copy semantics
     Tree (const Tree<T,W>& t):   //è da implementare la costruzione mediante il metodo insert
@@ -312,10 +320,29 @@ public: //forse va cancellato
         last{nullptr},
         size_tree{0}  {
             ctr_insert(*this,t.root);
-            
             }
-   
+    
+    Tree& operator=(const Tree<T,W>& t) {
+        Clear();
+        ctr_insert(*this,t.root);
+        return *this;
+    }
+    
+    Tree& operator=(Tree<T,W>&& t) {
+        Clear();
+        root=t.root;
+        first=t.first;
+        last=t.last;
+        size_tree=t.size_tree;
+        t.root=nullptr;
+        t.first=nullptr;
+        t.last=nullptr;
+        t.size_tree=0;
+        return *this;
+    }
 
+
+private:    
 
     void recursive_clear(Node* n){
         Node* l{n->left};
@@ -325,7 +352,10 @@ public: //forse va cancellato
         if(l==nullptr and r==nullptr) return;
         if(l!=nullptr) recursive_clear(l);
         if(r!=nullptr) recursive_clear(r);
-        }
+        }  
+    
+    
+public:   
     void Clear() {
         if(size_tree==0) return;
         Node* n{root};
@@ -343,51 +373,144 @@ public: //forse va cancellato
     
     
 
-void rec_balance(int s, Tree<T,W>& b, Tree<T,W>::iterator m, int lun) {
-    
-    b.Insert(*m,9999);
-    Node* p{b.Root()};
-    /*if (b.Size()>9){
-    Constiterator iter{b.First()};
-    cout<<"Albero provvisorio"<<endl;
-    for(int k{1};k<10;k++) {cout<<*iter<<" "<<" parent"<<!iter<<endl; ++iter;}
-    cout<<endl;};*/
-    if(b.Size()==s) return;
-    
-    int lun_l{lun/2};
-    int lun_r{lun-1-lun_l};
-    iterator i{m};
-    iterator j{m};
-    
-    if(lun_l>0) {
-        for(int ii{1};ii<=lun_l/2+lun_l%2;ii++) --i;
-        cout<<endl;
-        rec_balance(s,b,i,lun_l);
-        };
-    if(lun_r>0) {
-        for(int jj{0};jj<=lun_r/2;jj++) ++j;
-        rec_balance(s,b,j,lun_r);
-        };
-    return;  
-}
 
+
+
+
+
+
+private:
+    void rec_balance(int s, Tree<T,W>& b, Tree<T,W>::iterator m, int lun) {
+
+        b.Insert(*m,!m);
+        Node* p{b.Root()};
+        if(b.Size()==s) return;
+
+        int lun_l{lun/2};
+        int lun_r{lun-1-lun_l};
+        iterator i{m};
+        iterator j{m};
+
+        if(lun_l>0) {
+            for(int ii{1};ii<=lun_l/2+lun_l%2;ii++) --i;
+            rec_balance(s,b,i,lun_l);
+            };
+        if(lun_r>0) {
+            for(int jj{0};jj<=lun_r/2;jj++) ++j;
+            rec_balance(s,b,j,lun_r);
+            };
+        return;  
+    } 
+
+    void Bal_reinsert(Node* n,Node* r) {
+        Node* p{r};
+        while(!(p->left ==nullptr and p->right==nullptr) and !
+                (p->left==nullptr and n->key < p->key) and 
+                !(p->right==nullptr and n->key > p->key)) 
+            {
+            if(n->key<p->key) {p=p->left;} 
+            else {p=p->right;};      
+                };
+            if(n->key<p->key) {p->left=n;    n->parent=p; }       //aggiorna il ramo sinistro di p
+            else         {p->right=n;   n->parent=p; };          //aggiorna il ramo destro di p
+        } 
+        
+    void left_branch_deattaching(Node* a, Node* m){
+        m->parent->right=nullptr;
+        m->parent=a->parent;
+        if(a->parent->key < m->key) {a->parent->right=m;}
+        else {a->parent->left=m;};
+        a->parent=nullptr;
+    }
+
+    void right_branch_deattaching(Node* a, Node* m) {
+        m->parent->left=nullptr;
+        m->parent=a->parent;
+        if(a->parent->key < m->key) {a->parent->right=m;}
+        else {a->parent->left=m;};
+        a->parent=nullptr;
+    }
+
+
+
+
+
+    void Rec_Balance(Node* m, Node* r, int lun){
+        if(m!=r){
+        while (m->parent != r) {
+            Node* a{m};
+            if(a->parent->key < m->key){
+                while (a->parent->key < m->key  and a->parent != r) a=a->parent;
+                left_branch_deattaching(a,m);
+                Bal_reinsert(a,m);
+                a=m;
+                };
+            if (a->parent != r) {
+                while (a->parent->key > m->key  and a->parent != r) a=a->parent;
+                right_branch_deattaching(a,m);
+                Bal_reinsert(a,m); 
+                a=m;  };
+            };
+            };
+            int lun_l{lun/2};
+            int lun_r{lun-1-lun_l};
+            iterator i{m};
+            iterator j{m};
+            
+            if(lun_l>0) {
+                for(int ii{1};ii<=lun_l/2+lun_l%2;ii++) --i;
+                Rec_Balance(i.node(),m,lun_l);
+                };
+            if(lun_r>0) {
+                for(int jj{0};jj<=lun_r/2;jj++) ++j;
+                Rec_Balance(j.node(),m,lun_r);
+                };
+        return;  
+
+} 
+public: 
 
 void balance() {
-    cout<<root->key<<endl;
+
     Tree<T,W> b;
     iterator i{first};
     for(int ii{1};ii<size_tree/2+1;ii++) ++i;
     rec_balance(size_tree,b,i,size_tree);
+    *this=std::move(b);}
+
+
+void Balance(){
     
-    root=b.Root();
-    first=b.First();
-    last=b.Last();
-    cout<<root->key<<endl;
-    cout<<first->key<<endl;
-    cout<<last->key<<endl;
     
+    if(size_tree<2) return;
+    iterator i{first};
+    for(int ii{1};ii<size_tree/2+1;ii++) ++i;   //cerca la mediana
+    if(i.node()->parent!=root){ //fino al nodo sotto la root
+        while (i.node()->parent!= root) {
+            Node* m{i.node()};  //mediana
+            Node* a{i.node()};  //mediana per iterare
+            if(a->parent->key < m->key){
+                while (a->parent->key < m->key  and a->parent!= root) a=a->parent; //risale a sinistra fino a quando possibile
+                left_branch_deattaching(a,m); //stacca il pezzo da m ad a, riattacca m a a->parent
+                Bal_reinsert(a,m); //reinserisce il pezzo reinserendo a
+                a=m;  //aggiorna a
+                };
+            if (a->parent!= root) {
+                while (a->parent->key > m->key  and a->parent!= root) a=a->parent;
+                right_branch_deattaching(a,m);
+                Bal_reinsert(a,m); 
+                a=m;  };
+            };
+        };
+     //ora che la mediana è sotto la root agisco sulla root
+    i.node()->parent=nullptr; 
+    if(i.node()->key<root->key) {root->left=nullptr;}
+    else {root->right=nullptr;};
+    Bal_reinsert(root,i.node()); //reinserisco la root
+    root=i.node(); //ridefinisco la root
+    Rec_Balance(i.node(),root,size_tree);
 }
-    
+  
     
     
 };
@@ -398,9 +521,10 @@ std::ostream& operator<<(std::ostream& os, const Tree<T,W>& l) {
     else {
     typename Tree<T,W>::Constiterator j = l.first;
     for(; j!=l.last;++j){
-        os <<"key: "<<*j<<"   parent: " <<!j<<endl;
+        
+        os <<"key: "<<*j<<"   value: " <<!j<<"      parent: "<<j.parent_key()<<endl;
         }
-    os <<"key: "<<*j<<"   parent: " <<!j<<endl;
+    os <<"key: "<<*j<<"   value: " <<!j<<"      parent: "<<j.parent_key()<<endl;
 
     return os ;}
 }
@@ -411,30 +535,21 @@ std::ostream& operator<<(std::ostream& os, const Tree<T,W>& l) {
 int main() {
 
 
-
+std::vector<int> indici{43, 21, 23, 7, 2, 26, 49, 11, 5, 60, 45, 61, 41, 56, 30, 57, 46, 17, 53, 14, 33, 31, 15, 38, 29, 6, 59, 12, 3, 28, 36, 52, 13, 55, 54, 20, 39, 1, 44, 40, 48, 8, 35, 32, 47, 62, 51, 18, 25, 0, 58, 19, 16, 50, 42, 4, 10, 34, 37, 24, 22, 27, 9};
 
 
 Tree<int, long long int> A;
-A.Insert(1,1);
-A.Insert(2,2);
-A.Insert(3,3);
-A.Insert(4,5);
-A.Insert(5,0);
-A.Insert(6,0);
-A.Insert(7,0);
-A.Insert(8,0);
-A.Insert(9,0);
-A.Insert(10,0);
-A.Insert(11,0);
-A.Insert(12,0);
-A.Insert(15,0);
-A.Insert(14,0);
-A.Insert(13,0);
-//cout<<A<<endl;
+for(int i{1};i<16;i++) {A.Insert(i,0);};
+cout<<A<<endl;
 A.print();
-A.balance();
+A.Balance();
 A.print();
-cout<<endl<<A<<endl;
+cout<<endl<<"++++++++++++++"<<endl <<A<<endl;
+int height{0};
+//for (int i{0};i<=5;i++) {height+=pow(2,i);cout<<" "<<height<<endl;};
+
+p=A.find(100)
+while (p!=A.cend()) {cout<<*p<<endl;p++;};
 
 
 }
